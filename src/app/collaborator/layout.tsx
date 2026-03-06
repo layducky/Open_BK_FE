fa4e2d2f2680ee257fe5bacff21bc2a1e291889f
layducky <tranphuocnhatpnt@gmail.com>
2026-03-06 13:25:46 +0700

    fix: transition error

--- src/app/collaborator/layout.tsx
+++ src/app/collaborator/layout.tsx
@@ -19,7 +19,9 @@ export default function DashboardLayout({
         <div className="dashboard-bottom flex flex-row w-[80vw] rounded-2xl h-fit relative">
           <div className="w-full h-full absolute left-0 top-0 border-[1.5px] border-[#E3E2E6] rounded-2xl -z-10 bg-[#FAF9FD]"></div>
           <LeftBar />
-          <div className="p-8 w-fit h-fit flex-1">{children}</div>
+          <div className="p-8 w-fit h-fit flex-1">
+            {children}
+          </div>
         </div>
       </div>
     </main>
